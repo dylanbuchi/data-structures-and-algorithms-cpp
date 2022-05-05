4f001bb2a973c59cbd8b32e1bf506e3dd6ee4263
Dylan Buchi <devybuchi@gmail.com>
2022-05-05 07:54:39 -0300

    Update `static_arrays.cpp`

--- data_structures/arrays/static_arrays.cpp
+++ data_structures/arrays/static_arrays.cpp
@@ -1,29 +1,8 @@
 #include <iostream>
+#include <print.hh>
 
 using std::cout;
 using std::endl;
-
-template <typename Object>
-void print(Object object) {
-    cout << object << endl;
-}
-
-template <typename Object>
-void print_memory_address(Object object) {
-    cout << &object << endl;
-}
-
-void print() {
-    cout << endl;
-}
-
-void print_array(int array[], int size) {
-    cout << "[";
-    for (size_t i = 0; i < size; i++) {
-        cout << array[i] << (i < size - 1 ? ", " : "");
-    }
-    cout << "]";
-}
 
 // resize the array in place
 void resize_array(int*& old_array, size_t& old_array_size) {
