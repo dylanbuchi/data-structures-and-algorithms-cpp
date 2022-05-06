98890ad60bd6bef0ce1135f7880486d3f1d5dd18
Dylan Buchi <devybuchi@gmail.com>
2022-05-06 08:34:39 -0300

    Add function `print_2d_array`

--- modules/print.hh
+++ modules/print.hh
@@ -14,7 +14,7 @@ void print_memory_address(Object object) {
 }
 // prints a new line
 void print() {
-        cout << endl;
+    cout << endl;
 }
 
 void print_array(int array[], int size) {
@@ -23,4 +23,16 @@ void print_array(int array[], int size) {
         cout << array[i] << (i < size - 1 ? ", " : "");
     }
     cout << "]";
+}
+
+void print_2d_array(int** array, size_t row_size, size_t col_size) {
+    for (size_t row = 0; row < row_size; row++) {
+        cout << "[";
+        for (size_t col = 0; col < col_size; col++) {
+            cout << array[row][col] << (col < col_size - 1 ? ", " : "");
+        }
+        cout << "]";
+
+        print();
+    }
 }
