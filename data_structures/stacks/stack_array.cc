96093e24e898e432537f85cf8f7fd1049f5f9085
Dylan Buchi <devybuchi@gmail.com>
2022-05-27 18:14:25 -0300

    Add constructors, destructor to class `StackArray`

--- data_structures/stacks/stack_array.cc
+++ data_structures/stacks/stack_array.cc
@@ -1,13 +1,44 @@
+#include "exception.h"
 #include "list.h"
+#include "print.h"
 
 template <class T>
-class StackArray : public IList<T> {
+class StackArray {
 private:
-    T* stack;
-    int top;
+    T* stack_;
+    int top_;
+    size_t capacity_;
 
 protected:
     size_t size;
+    Exception error;
 
 public:
+    StackArray() : StackArray(10) {}
+    StackArray(size_t);
+    ~StackArray();
+
+    size_t length();
 };
+
+template <class T>
+StackArray<T>::StackArray(size_t size) {
+    if (size < 1)
+        throw Exception(error.kInvalidValue);
+
+    stack_ = new T[size];
+    capacity_ = size;
+
+    top_ = -1;
+    size = 0;
+}
+
+template <class T>
+StackArray<T>::~StackArray() {
+    delete[] stack_;
+}
+
+template <class T>
+size_t StackArray<T>::length() {
+    return size;
+}
