5ff9e606227e3289f05b389d3006944a4a8c0054
Dylan Buchi <devybuchi@gmail.com>
2022-05-27 05:26:06 -0300

    Add methods `PopBack`, `PopFront`

--- data_structures/linked_lists/doubly_linkedlist.cc
+++ data_structures/linked_lists/doubly_linkedlist.cc
@@ -8,6 +8,9 @@ public:
     void Display();
     void PushBack(T);
     void PushFront(T);
+
+    T PopFront();
+    T PopBack();
 };
 
 template <class T>
@@ -57,4 +60,52 @@ void DoublyLinkedList<T>::PushFront(T data) {
     }
 
     this->size++;
+}
+
+template <class T>
+T DoublyLinkedList<T>::PopFront() {
+    this->HandleEmptyLinkedList();
+
+    T deleted = this->head->data;
+
+    if (this->size == 1) {
+        delete this->head;
+        this->head = this->tail = nullptr;
+
+    } else {
+        auto* next_node = this->head->next;
+
+        this->head->next = next_node->prev = nullptr;
+
+        delete this->head;
+
+        this->head = next_node;
+    }
+
+    this->size--;
+
+    return deleted;
+}
+
+template <class T>
+T DoublyLinkedList<T>::PopBack() {
+    this->HandleEmptyLinkedList();
+
+    T deleted = this->tail->data;
+
+    if (this->length() == 1)
+        return PopFront();
+
+    else {
+        auto* previous_node = this->tail->prev;
+        previous_node->next = this->tail->prev = nullptr;
+
+        delete this->tail;
+
+        this->tail = previous_node;
+    }
+
+    this->size--;
+
+    return deleted;
 }
