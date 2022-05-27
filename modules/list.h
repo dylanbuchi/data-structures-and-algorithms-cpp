44fd474187cdcb3c51f4e8901529d3f52652d3a9
Dylan Buchi <devybuchi@gmail.com>
2022-05-27 18:51:02 -0300

    Fix list.h

--- modules/list.h
+++ modules/list.h
@@ -10,10 +10,10 @@ public:
     virtual bool Empty() = 0;
     virtual bool Full() = 0;
 
-    virtual void Push() = 0;
+    virtual void Push(T) = 0;
     virtual void Display() = 0;
 
-    virtual size_t length();
+    virtual size_t length() = 0;
 
     virtual T Pop() = 0;
     virtual T Peek() = 0;
