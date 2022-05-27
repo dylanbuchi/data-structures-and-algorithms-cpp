8e3bc16d3efeb55b5ebe24c1e8bd1bb50bae386f
Dylan Buchi <devybuchi@gmail.com>
2022-05-27 03:34:25 -0300

    Add `prev` member to Node class, update destructor

--- modules/node.h
+++ modules/node.h
@@ -5,14 +5,24 @@ class Node {
 public:
     T data;
     Node<T> *next;
+    Node<T> *prev;
 
     Node(T data) {
         this->data = data;
+
         this->next = nullptr;
+        this->prev = nullptr;
     }
 
     ~Node() {
+        auto *temp = next;
         delete next;
-        next = nullptr;
+
+        if (temp != prev) {
+            delete prev;
+            prev = nullptr;
+        }
+
+        temp = next = nullptr;
     };
 };
