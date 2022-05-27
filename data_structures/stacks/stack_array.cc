1879378f324cbac04cf0294f282f8c1f1428e242
Dylan Buchi <devybuchi@gmail.com>
2022-05-27 19:02:27 -0300

    Add functions of Interface `IList` in `StackArray`

--- data_structures/stacks/stack_array.cc
+++ data_structures/stacks/stack_array.cc
@@ -3,31 +3,46 @@
 #include "print.h"
 
 template <class T>
-class StackArray {
+class StackArray : public IList<T> {
 private:
-    T* stack_;
-    int top_;
     size_t capacity_;
 
+    int top_;
+
+    T* stack_;
+
 protected:
-    size_t size;
     Exception error;
+
+    size_t size;
+
+    void HandleEmptyStack();
 
 public:
     StackArray() : StackArray(10) {}
     StackArray(size_t);
+
     ~StackArray();
 
     size_t length();
+
+    bool Empty();
+    bool Full();
+
+    void Display();
+    void Push(T);
+
+    T Pop();
+    T Peek();
 };
 
 template <class T>
-StackArray<T>::StackArray(size_t size) {
-    if (size < 1)
+StackArray<T>::StackArray(size_t initial_capacity) {
+    if (initial_capacity < 1)
         throw Exception(error.kInvalidValue);
 
-    stack_ = new T[size];
-    capacity_ = size;
+    stack_ = new T[initial_capacity];
+    capacity_ = initial_capacity;
 
     top_ = -1;
     size = 0;
@@ -41,4 +56,50 @@ StackArray<T>::~StackArray() {
 template <class T>
 size_t StackArray<T>::length() {
     return size;
+}
+
+template <class T>
+bool StackArray<T>::Empty() {
+    return not size;
+}
+
+template <class T>
+bool StackArray<T>::Full() {
+    return size == capacity_;
+}
+
+template <class T>
+void StackArray<T>::Display() {
+    PrintArray(stack_, size);
+}
+
+template <class T>
+void StackArray<T>::Push(T item) {
+    if (not Full()) {
+        stack_[++top_] = item;
+        size++;
+    }
+}
+
+template <class T>
+T StackArray<T>::Pop() {
+    HandleEmptyStack();
+
+    T deleted = stack_[top_--];
+    size--;
+
+    return deleted;
+}
+
+template <class T>
+T StackArray<T>::Peek() {
+    HandleEmptyStack();
+
+    return stack_[top_];
+}
+
+template <class T>
+void StackArray<T>::HandleEmptyStack() {
+    if (Empty())
+        throw Exception(error.kEmpty);
 }
