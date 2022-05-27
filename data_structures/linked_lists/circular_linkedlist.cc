7e5d832792731cb667d03333797ca089f8ea0cda
Dylan Buchi <devybuchi@gmail.com>
2022-05-26 23:56:47 -0300

    Fix method ´Display´ and add method `PushBack`

--- data_structures/linked_lists/circular_linkedlist.cc
+++ data_structures/linked_lists/circular_linkedlist.cc
@@ -3,6 +3,8 @@
 #include "node.h"
 #include "print.h"
 
+using std::cout;
+
 template <class T>
 class CircularLinkedList : public IList<T> {
 private:
@@ -21,6 +23,8 @@ public:
     bool Empty();
 
     size_t length();
+
+    void PushBack(T);
 };
 
 template <class T>
@@ -49,7 +53,15 @@ CircularLinkedList<T>::~CircularLinkedList() {
 
 template <class T>
 void CircularLinkedList<T>::Display() {
-    PrintLinkedList(head_);
+    auto *temp = head_;
+
+    do {
+        cout << temp->data << " --> ";
+        temp = temp->next;
+
+    } while (temp != head_);
+
+    Print();
 }
 
 template <class T>
@@ -60,4 +72,22 @@ bool CircularLinkedList<T>::Empty() {
 template <class T>
 size_t CircularLinkedList<T>::length() {
     return length_;
+}
+
+template <class T>
+void CircularLinkedList<T>::PushBack(T data) {
+    Node<T> *new_node = new Node{data};
+
+    if (Empty()) {
+        new_node->next = new_node;
+        head_ = tail_ = new_node;
+    }
+
+    else {
+        tail_->next = new_node;
+        tail_ = new_node;
+        tail_->next = head_;
+    }
+
+    length_++;
 }
