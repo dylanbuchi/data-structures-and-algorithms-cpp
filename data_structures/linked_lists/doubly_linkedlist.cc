7663becc445aa5b4f0b1f35a2d2e98000bc93c4e
Dylan Buchi <devybuchi@gmail.com>
2022-05-27 04:43:54 -0300

    Add class `DoublyLinkedList`

---
+++ data_structures/linked_lists/doubly_linkedlist.cc
@@ -0,0 +1,42 @@
+#include "linkedlist.cc"
+
+using std::cout;
+
+template <class T>
+class DoublyLinkedList : public LinkedList<T> {
+public:
+    void Display();
+    void PushBack(T);
+};
+
+template <class T>
+void DoublyLinkedList<T>::Display() {
+    this->HandleEmptyLinkedList();
+
+    auto* temp = this->head;
+
+    cout << "[";
+    while (temp) {
+        cout << temp->data << " <==> ";
+        temp = temp->next;
+    }
+
+    Print("]");
+}
+
+template <class T>
+void DoublyLinkedList<T>::PushBack(T data) {
+    Node<T>* new_node = new Node{data};
+
+    if (this->Empty())
+        this->head = this->tail = new_node;
+
+    else {
+        new_node->prev = this->tail;
+
+        this->tail->next = new_node;
+        this->tail = new_node;
+    }
+
+    this->size++;
+}
